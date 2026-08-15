753121ad91905d116e2871c91387ff5cf94daf78
Gidds <giddsv@gmail.com>
2026-08-15 20:11:45 +0200

    Route occurrence entries

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\OccurrenceEntryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response('OB Entry Book Laravel rebuild OK', 200);
-});
+Route::get('/', [OccurrenceEntryController::class, 'index'])->name('entries.index');
+Route::get('/entries/create', [OccurrenceEntryController::class, 'create'])->name('entries.create');
+Route::post('/entries', [OccurrenceEntryController::class, 'store'])->name('entries.store');
