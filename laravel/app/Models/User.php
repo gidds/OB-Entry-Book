ba27d392a70ec13cb7a0d3a89abf73411d79cee7
Gidds <giddsv@gmail.com>
2026-08-15 20:13:50 +0200

    Add user model

---
+++ laravel/app/Models/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'username',
+        'password',
+        'role',
+        'pin_hash',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'pin_hash',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
+    public function isManagement(): bool
+    {
+        return in_array($this->role, ['manager', 'admin'], true);
+    }
+}
